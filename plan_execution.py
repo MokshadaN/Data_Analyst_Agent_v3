ac677521fd15b014f854a49054290bd0efcf607d
Mokshada Nehete <147795188+MokshadaN@users.noreply.github.com>
2025-08-15 08:02:25 +0530

    Update plan_execution.py

--- plan_execution.py
+++ plan_execution.py
@@ -3,10 +3,10 @@ import os
 import subprocess
 import tempfile
 
-from llm_calls.claude_call import claude_call_for_code
+# from llm_calls.claude_call import claude_call_for_code
 from prompts import PromptManager
 from llm_calls.gemini_llm import gemini_call_for_code  # updated import
-from llm_calls.openai_call import openai_call_for_code_responses
+# from llm_calls.openai_call import openai_call_for_code_responses
 import time
 from dotenv import load_dotenv
 load_dotenv()
@@ -175,4 +175,5 @@ def _build_repair_prompt(system_prompt, plan,questions, data_files, prev_code, e
     )
 
     return repair_system_prompt, repair_user_prompt
+
 
